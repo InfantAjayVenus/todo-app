7cd65eb6218ec17fe972c3a01e60d8f9049474de
InfantAjayVenus <infantcsajay@outlook.com>
2024-04-16 15:46:49 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Helmet from 'react-helmet';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>todo.</title>
+      </Helmet>
+      <Typography>todo.</Typography>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import '@fontsource/lexend/400.css';
+import '@fontsource/lexend/500.css';
+import '@fontsource/lexend/700.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
