fa72388fab7b736d6a8af6da242de3091147e64c
InfantAjayVenus <infantcsajay@outlook.com>
2024-04-18 19:55:22 +0530

    [app] setup: base theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { CssBaseline, ThemeOptions, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Helmet from 'react-helmet';
-import { Typography } from '@mui/material';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#F78079',
+    },
+    secondary: {
+      main: '#22bec7',
+    },
+    background: {
+      default: 'rgb(228, 240, 250)',
+      paper: 'rgb(230, 238, 248)',
+    },
+    text: {
+      primary: 'rgb(43, 60, 91)',
+      secondary: 'rgb(122, 138, 163)',
+      disabled: 'rgb(196, 205, 209)',
+    },
+  },
+};
+
+const lightTheme = createTheme(themeOptions);
 
 function App() {
   return (
@@ -7,7 +30,12 @@ function App() {
       <Helmet>
         <title>todo.</title>
       </Helmet>
-      <Typography>todo.</Typography>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <head>
+            <Typography>todo.</Typography>
+        </head>
+      </ThemeProvider>
     </>
   );
 }
