9e1c098b9bd51d70c34fb1308fcd97eb3ec51042
Infant Ajay Venus <infantcsajay@outlook.com>
2024-05-01 23:14:08 +0530

    [Sections] patch: add settings button

--- src/pages/Sections.tsx
+++ src/pages/Sections.tsx
@@ -1,21 +1,27 @@
+import { Button, Stack } from "@mui/material";
 import Filters from "../components/Filters";
 import Projects from "../components/Projects";
+import { Settings } from "@mui/icons-material";
 
 export default function Sections() {
-    
+
     return (
-        <aside
-            style={{
+        <Stack
+            sx={{
                 height: '80vh',
                 width: '17rem'
             }}
+            justifyContent={'space-between'}
         >
-            <section>
-                <Filters />
-            </section>
-            <section>
-                <Projects />
-            </section>
-        </aside>
+            <div>
+                <section>
+                    <Filters />
+                </section>
+                <section>
+                    <Projects />
+                </section>
+            </div>
+            <Button><Settings /> Settings</Button>
+        </Stack>
     )
 }
