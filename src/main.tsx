3a68442d83669e0dedd0293d9e1525d635dd92dd
Infant Ajay Venus <infantcsajay@outlook.com>
2024-06-20 22:54:35 +0530

    [main] feat: bootstrap styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import '@fontsource/lexend/400.css';
-import '@fontsource/lexend/500.css';
-import '@fontsource/lexend/700.css';
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
