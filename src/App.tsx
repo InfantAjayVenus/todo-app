2a9e78db6d89a0dbaa7908cb527b70f739c6ad20
Infant Ajay Venus <infantcsajay@outlook.com>
2024-06-20 22:55:05 +0530

    [app] feat: create base task list UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,33 @@
-import { Container, CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Helmet from 'react-helmet';
-import { createContext, useState } from 'react';
-import { ThemeValue } from './app/types/Themes';
-import useSystemTheme from './app/hooks/useSystemTheme';
-import Home from './app/pages/Home';
+import { Button } from './components/ui/button';
+import { LuPlus } from 'react-icons/lu';
+import { Checkbox } from './components/ui/checkbox';
 
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#F78079',
-    },
-    secondary: {
-      main: '#22bec7',
-    },
-    background: {
-      default: 'rgb(228, 240, 250)',
-      paper: 'rgb(230, 238, 248)',
-    },
-    text: {
-      primary: 'rgb(43, 60, 91)',
-      secondary: 'rgb(122, 138, 163)',
-      disabled: 'rgb(196, 205, 209)',
-    },
-  },
-  typography: {
-    fontFamily: 'lexend',
-  },
-});
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#815ef7',
-    },
-    background: {
-      default: '#121215',
-      paper: '#1d1a28',
-    },
-    text: {
-      primary: '#cdb6fd',
-      secondary: '#929098',
-      disabled: '#717172',
-    }
-  },
-  typography: {
-    fontFamily: 'lexend',
-  },
-})
-
-
-export const ThemeValueContext = createContext({themeValue: ThemeValue.SYSTEM, setThemeValue: (_:any) => {} });
 
 function App() {
-  const [themeValue, setThemeValue] = useState<ThemeValue>(ThemeValue.SYSTEM);
-  const isSystemThemeLight = useSystemTheme();
-  const isThemeLight = themeValue === ThemeValue.SYSTEM ? isSystemThemeLight : themeValue === ThemeValue.LIGHT;
 
   return (
     <>
       <Helmet>
         <title>todo.</title>
       </Helmet>
-      <ThemeProvider theme={isThemeLight ? lightTheme : darkTheme}>
-        <CssBaseline />
-        <Container
-          sx={{
-            padding: '2rem 3rem'
-          }}
-        >
-          <header>
-            <Typography variant='h3' fontWeight={'700'} color='primary.main'>todo.</Typography>
-          </header>
-          <ThemeValueContext.Provider 
-            value={{
-              themeValue,
-              setThemeValue
-            }}
-          >
-            <Home />
-          </ThemeValueContext.Provider>
-        </Container>
+      <main className='h-screen w-screen px-4 py-6'>
+        <div className='relative border border-current rounded-3xl min-h-[44rem] p-4'>
+          <ul className='py-6'>
+            {(new Array(5).fill(0).map(() => (
+              <li className='flex items-center justify-between border-b p-2 mb-2 capitalize'>
+                <p className='text-lg'>test</p>
+                <Checkbox className='h-5 w-5' />
+              </li>
+            )))}
 
-      </ThemeProvider>
+
+          </ul>
+          <Button variant={'outline'} className='rounded-xl border-current absolute bottom-4 right-4 h-12 w-12 p-1 text-2xl '>
+            <LuPlus />
+          </Button>
+        </div>
+      </main>
     </>
   );
 }
